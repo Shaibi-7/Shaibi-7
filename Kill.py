a44f0d18641e405a6394fe3e8aa98636395d4c66
Shaibi-7 <81957443+Shaibi-7@users.noreply.github.com>
2021-04-05 13:58:46 +0500

    Create Kill.py

---
+++ Kill.py
@@ -0,0 +1,6 @@
+- 👋 Hi, I’m @Shaibi-7
+- 👀 I’m interested in hacking
+<!---
+Shaibi-7/Shaibi-7 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
